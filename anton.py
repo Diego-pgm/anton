9989648114a29ca4cff7a0d725861df02cb12128
Diego Pérez Gramond Muñoz <diego.pegramm@gmail.com>
2022-01-27 01:13:01 -0600

    Anton now can convert still in process,

--- anton.py
+++ anton.py
@@ -92,6 +92,68 @@ def jokes():
     joke = pyjokes.get_joke()
     speak(joke)
 
+
+
+def menu():
+    os.system('cls')
+    print('=====================Anton Number Converter==========================')
+    print('Menu Options:\n\
+        1) Binary to Decimal\n\
+        2) Decimal to Binary\n\
+        3) Binary to Hex\n\
+        4) Hex to Binary\n\
+        5) Decimal to Hex\n\
+        6) Hex to Decimal\n\
+        0) Exit')
+    opt = int(input('anton> '))
+    return opt
+
+
+
+def convert(opt):
+    os.system('cls')
+    result = 0
+    num = 1
+    tipo ="nada"
+    if opt == 1:
+        num = input('\n[+] Please enter the binary number\nanton> ')
+        result = int(num, 2)
+        tipo = 'decimal'
+        return result, num, tipo 
+    elif opt == 2:
+        num = input('\n[+] Please enter the decimal number\nanton> ')
+        result = bin(int(num))
+        tipo = 'binary'
+        return result, num, tipo 
+    elif opt == 3:
+        num = input('\n[+] Please enter the binary number\nanton> ')
+        result_dec = int(num,2)
+        result = hex(result_dec)
+        tipo = 'hexadecimal'
+        return result, num, tipo 
+    elif opt == 4:
+        num = input('\n[+] Please enter the hexadecimal number\nanton> ')
+        result_dec = int(num,16)
+        result = bin(result_dec)
+        tipo = 'binary'
+        return result, num, tipo 
+    elif opt == 5:
+        num = input('\n[+] Please enter the decimal number\nanton> ')
+        result = hex(int(num))
+        tipo = 'hexadecimal'
+        return result, num, tipo 
+    elif opt == 6:
+        num = input('\n[+] Please enter the hexadecimal number\nanton> ')
+        result = int(num,16)
+        tipo = 'decimal'
+        return result, num, tipo 
+    else:
+        print('[-] Please enter a valid option')
+        return result, num, tipo
+    
+    
+    
+
 if __name__ == "__main__":
     greeting()
     while True:
@@ -194,3 +256,9 @@ if __name__ == "__main__":
         elif 'offline' in query:
             speak('Disconected.')
             quit()
+
+        elif 'manager' in query:
+            opt = menu()
+            result, num, tipo = convert(opt)
+            print('[+] The number {} is {} in {}\n'.format(num, result, tipo))
+            input('PRESS ENTER TO CONTINUE')
